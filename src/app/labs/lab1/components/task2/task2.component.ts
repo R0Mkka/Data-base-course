dd2845456aeed6c1b83353f1648dacb24ba90d88
Roman Aleksanov <alexanov.roman@gmail.com>
2019-09-22 12:01:01 +0300

    Lab1 Task2 was finished

--- src/app/labs/lab1/components/task2/task2.component.ts
+++ src/app/labs/lab1/components/task2/task2.component.ts
@@ -9,7 +9,9 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 })
 export class Task2Component implements OnInit {
   public userNumberControl: FormControl;
-  public randomNumberControl: FormControl;
+  public resultControl: FormControl;
+  public guessedNumber = '6';
+  public parallelepipedVolumeForm: FormGroup;
 
   constructor(
     private readonly formBuilder: FormBuilder
@@ -20,11 +22,44 @@ export class Task2Component implements OnInit {
   }
 
   public showResult(): void {
+    if (this.userNumberControl.value.length === 0) {
+      this.resultControl.setValue('Вы не ввели число.');
 
+      return;
+    }
+
+    const resultText = this.userNumberControl.value === this.guessedNumber
+      ? 'Угадал!'
+      : 'Не угадал.';
+
+    this.resultControl.setValue(resultText);
+  }
+
+  public showParallelepipedVolume(): void {
+    const resultControl = this.parallelepipedVolumeForm.get('result');
+
+    if (!this.parallelepipedVolumeForm.valid) {
+      resultControl.setValue('Указаны не все параметры.');
+
+      return;
+    }
+
+    const length = this.parallelepipedVolumeForm.get('length').value;
+    const width = this.parallelepipedVolumeForm.get('width').value;
+    const height = this.parallelepipedVolumeForm.get('height').value;
+
+    resultControl.setValue(`${(length * width * height).toString()} см2`);
   }
 
   private initForms(): void {
     this.userNumberControl = this.formBuilder.control('');
-    this.randomNumberControl = this.formBuilder.control('');
+    this.resultControl = this.formBuilder.control('');
+
+    this.parallelepipedVolumeForm = this.formBuilder.group({
+      length: ['', Validators.required],
+      width: ['', Validators.required],
+      height: ['', Validators.required],
+      result: ['']
+    });
   }
 }
